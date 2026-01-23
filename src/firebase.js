954c57ec9f94a2f59a129fcf085d54e9d21812dd
abermudinventario-tech <abermudinventario@gmail.com>
2026-01-23 00:18:50 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,7 @@
+// src/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-// Tu configuración de Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyAmZ6BDIrvipQH3alSF3fu-W3QHMIs8V3E",
-  authDomain: "abermud-7bf0c.firebaseapp.com",
-  projectId: "abermud-7bf0c",
-  storageBucket: "abermud-7bf0c.firebasestorage.app",
-  messagingSenderId: "592064738542",
-  appId: "1:592064738542:web:b33c050136449d49466ff"
-};
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-
-// Tu configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAmZ6BDIrvipQH3alSF3fu-W3QHMIs8V3E",
   authDomain: "abermud-7bf0c.firebaseapp.com",
@@ -23,8 +11,5 @@ const firebaseConfig = {
   appId: "1:592064738542:web:b33c050136449d49466ff"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-
-// Inicializar Firestore
 export const db = getFirestore(app);
