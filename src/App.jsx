2e4b8a24dec5e3bfd20f4fe0b11809ff4822475c
abermudinventario-tech <abermudinventario@gmail.com>
2026-01-30 19:38:03 -0500

    Update App.jsx
    final

--- src/App.jsx
+++ src/App.jsx
@@ -3424,10 +3424,10 @@ doc.autoTable({
                   <div className="text-center py-8 bg-gray-50 rounded-lg border">
                     <p className="text-gray-500 text-sm">Carrito vacío</p>
                   </div>
-	            </div>
-       	      </div>
-       	    </div>
-   	  	  )}
+                </div>
+              </div>
+            </div>
+          )}	            
 
     {/* MODAL: Ver Venta */}
       {viewingSale && (
